86eaa976e26e7b48f0838c35aabcf701e3c28f14
Karan Singh <karans9609@gmail.com>
2024-03-27 21:20:27 +0530

    Applied Media Query for Different Sizes

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -4,11 +4,18 @@ import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown from '../Assets/dropdown_icon.png'
 
 const Navbar = () => {
 
   const [menu,setMenu] = useState("shop");
   const {getTotalCartItems} = React.useContext(ShopContext);
+  const menuRef = React.useRef();
+  
+  const dropdown_toggle = (e) =>{
+      menuRef.current.classList.toggle('nav-menu-visible');
+      e.target.classList.toggle('open'); 
+  }
 
   return (
     <div className='navbar'>
@@ -16,7 +23,8 @@ const Navbar = () => {
            <img src={logo} alt="" /> 
            <p>SHOPPER</p>
         </div>
-        <ul className="nav-menu">
+        <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+        <ul ref = {menuRef} className="nav-menu">
             <li onClick={() => {setMenu("shop")}}><Link style={{textDecoration: 'none'}} to ="/">Shop</Link>{menu === "shop"?<hr/>:<></>}</li>
             {/* Text decoration none removes the extra underline which was coming due to link tag */}
             <li onClick={() => {setMenu("men")}}><Link style={{textDecoration: 'none'}} to ="/men">Men</Link>{menu === "men"?<hr/>:<></>}</li>
